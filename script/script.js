142d4b77e215a502e10adf5fcc83fea2600082ef
VitaliyM <vetalmamchur@gmail.com>
2023-03-08 12:50:08 +0000

    8.3

--- script/script.js
+++ script/script.js
@@ -5,13 +5,14 @@ let asideLogo = document.querySelector('.aside-block__logo');
 
 mobileMenu.addEventListener('click', function() {
     asideBlock.classList.remove('aside-block-hover');
-    if (asideBlock.getBoundingClientRect().height < 420 + 'px') {
+    console.log(asideBlock.getBoundingClientRect().height);
+    // if (asideBlock.getBoundingClientRect().height < 420 + 'px') {
         asideBlock.style.height = 420 + 'px';
         asideManager.style.display = 'flex';
         asideLogo.style.paddingLeft = 135 + 'px';
 
-    } else {
-        asideBlock.style.height = 55 + 'px';
-        asideManager.style.display = 'none';
-    }
+    // } else {
+    //     asideBlock.style.height = 55 + 'px';
+    //     asideManager.style.display = 'none';
+    // }
 })
