e5c27e5ee370270389640b8c047846d7c02f043b
VitaliyM <vetalmamchur@gmail.com>
2023-03-08 13:13:18 +0000

    8.5

--- script/script.js
+++ script/script.js
@@ -1,18 +1,8 @@
-let mobileMenu = document.querySelector('.aside-logo__mobile-btn');
+let mobileMenuBtn = document.querySelector('.aside-logo__mobile-btn');
 let asideBlock = document.querySelector('.aside-block');
-let asideManager = document.querySelector('.aside-block__manager');
 let asideLogo = document.querySelector('.aside-block__logo');
+let asideManager = document.querySelector('.aside-block__manager');
 
-mobileMenu.addEventListener('click', function() {
-    asideBlock.classList.remove('aside-block-hover');
-    console.log(asideBlock.getBoundingClientRect().height);
-    if (asideBlock.getBoundingClientRect().height < 420 + 'px') {
-        asideBlock.style.height = 420 + 'px';
-        asideManager.style.display = 'flex';
-        asideLogo.style.paddingLeft = 135 + 'px';
-
-    } else {
-        asideBlock.style.height = 55 + 'px';
-        asideManager.style.display = 'none';
-    }
-})
+let showMenu = () => {
+    console.log("Hello");
+}
