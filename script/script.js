6b2df784821109608180a7ed86725b6d545dccfe
VitaliyM <vetalmamchur@gmail.com>
2023-03-08 15:14:05 +0000

    8.8

--- script/script.js
+++ script/script.js
@@ -3,8 +3,19 @@ let asideBlock = document.querySelector('.aside-block');
 let asideLogo = document.querySelector('.aside-block__logo');
 let asideManager = document.querySelector('.aside-block__manager');
 
+
 let showMenu = () => {
-    alert("Hello");
+    console.log(asideBlock.getBoundingClientRect().height);
+    if (asideBlock.getBoundingClientRect().height === 55) {
+        asideBlock.style.height = 420 + 'px';
+        asideLogo.style.paddingLeft = 135 + 'px';
+        asideManager.style.left = 10 + 'px';
+        asideManager.style.display = 'flex';
+    } else {
+        asideBlock.style.height = 55 + 'px';
+        asideLogo.style.paddingLeft = 30 + 'px';
+        asideManager.style.display = 'none';
+    }
 }
 
 mobileMenuBtn.onclick = showMenu;
